d904d7fe672b582884d239e2b4bbee059407771d
Sohaib Ali <sohaib.k@sofy.ai>
2025-12-22 17:53:42 +0500

    added init learning

---
+++ basics/init.go
@@ -0,0 +1,19 @@
+package basics
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initializing package1.....")
+}
+
+func init() {
+	fmt.Println("Initializing package2.....")
+}
+
+func init() {
+	fmt.Println("Initializing package3.....")
+}
+
+func init_learning() {
+	fmt.Println("Hello from init functions in GO")
+}
